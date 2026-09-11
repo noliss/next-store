1a5ae6a6891f87b8ffe07b4e1a0a5b9d36262e4d
Andrew <nolis920@gmail.com>
2026-09-11 13:25:05 +0300

    Конфигурация sass, изображения, пример env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'ohotaktiv.ru', pathname: '/upload/**' }],
+  },
+  sassOptions: {
+    loadPaths: [path.join(process.cwd(), 'src/shared/styles')],
+  },
 };
 
 export default nextConfig;
